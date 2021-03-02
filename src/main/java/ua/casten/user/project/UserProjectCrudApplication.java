1a9e944e07b4e8b3cab0ebd11fda720546363009
Oleksandr Borovyk <maingroon@gmail.com>
2021-03-02 08:13:48 +0200

    Created structure of project

---
+++ src/main/java/ua/casten/user/project/UserProjectCrudApplication.java
@@ -0,0 +1,13 @@
+package ua.casten.user.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProjectCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProjectCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/casten/user/project/UserProjectCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.casten.user.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProjectCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
