a3f5c57d722288e8468e8dfc7a45620f4dd05b50
Oleksandr Borovyk <maingroon@gmail.com>
2021-03-04 07:16:47 +0200

    Changed User entity

--- src/main/java/ua/casten/user/project/model/User.java
+++ src/main/java/ua/casten/user/project/model/User.java
@@ -3,10 +3,6 @@ package ua.casten.user.project.model;
 import lombok.Data;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -21,26 +17,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull(message = "Nickname cannot be null")
-    @Size(min = 4, max = 25, message = "Nickname must be between 4 and 25 characters")
-    @Pattern(regexp = "^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,24}[a-zA-Z0-9]$",
-            message = "Nickname should be valid")
     @Column(name = "nickname", length = 50, nullable = false, unique = true)
     private String nickname;
 
-    @Email(message = "Email should be valid")
-    @NotNull(message = "Email cannot be null")
-    @Size(min = 5, max = 50, message = "Email must be between 5 and 50 characters")
     @Column(name = "email", length = 50, nullable = false, unique = true)
     private String email;
 
-    @NotNull(message = "Password cannot be null")
     @Column(name = "password", length = 100, nullable = false)
     private String password;
 
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false)
-    @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    @JoinColumn(name = "role_id", nullable = false, updatable = false)
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "role_id", nullable = false)
     private Role role;
 }
