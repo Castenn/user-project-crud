7d222bee08701df41031fc7ee1799aca85ef9672
Oleksandr Borovyk <maingroon@gmail.com>
2021-03-22 20:16:57 +0200

    Added and changed repositories

--- src/main/java/ua/casten/user/project/model/repository/RoleRepository.java
+++ src/main/java/ua/casten/user/project/model/repository/RoleRepository.java
@@ -2,11 +2,8 @@ package ua.casten.user.project.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ua.casten.user.project.model.User;
+import ua.casten.user.project.model.Role;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findUserByNickname(String nickname);
-
+public interface RoleRepository extends JpaRepository<Role, Long> {
 }
