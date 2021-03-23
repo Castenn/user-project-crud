2396f0abd24c003e8d24f133850403188c47c1c2
Oleksandr Borovyk <maingroon@gmail.com>
2021-03-23 22:59:59 +0200

    Added update and delete functional

--- src/main/java/ua/casten/user/project/controller/UserController.java
+++ src/main/java/ua/casten/user/project/controller/UserController.java
@@ -3,10 +3,10 @@ package ua.casten.user.project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ua.casten.user.project.model.Role;
 import ua.casten.user.project.model.User;
+import ua.casten.user.project.service.RoleService;
 import ua.casten.user.project.service.UserService;
 
 import java.util.List;
@@ -16,24 +16,56 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final RoleService roleService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, RoleService roleService) {
         this.userService = userService;
+        this.roleService = roleService;
     }
 
     @GetMapping("")
-    public String getAll(Model model) {
+    public String all(Model model) {
         List<User> users = userService.getAll();
         model.addAttribute("users", users);
         return "user-list";
     }
 
     @GetMapping("{id}/read")
-    public String getUserInfo(@PathVariable Long id, Model model) {
+    public String info(@PathVariable Long id,
+                       Model model) {
         User user = userService.readById(id);
         model.addAttribute("user", user);
         return "user-read";
+    }
+
+    @GetMapping("{id}/update")
+    public String update(@PathVariable Long id,
+                         Model model) {
+        User user = userService.readById(id);
+        List<Role> roles = roleService.getAll();
+        model.addAttribute("user", user);
+        model.addAttribute("roles", roles);
+        return "user-update";
+    }
+
+    @PostMapping("{id}/update")
+    public String update(@PathVariable Long id,
+                         @ModelAttribute("user") User user,
+                         @RequestParam("oldPassword") String oldPassword,
+                         @RequestParam("roleId") Long roleId) {
+        User oldUser = userService.readById(id);
+        if (oldUser.getPassword().equals(oldPassword)) {
+            user.setRole(roleService.readById(roleId));
+            userService.save(user);
+        }
+        return "redirect:/users/" + id + "/read";
+    }
+
+    @GetMapping("{id}/delete")
+    public String delete(@PathVariable Long id) {
+        userService.delete(id);
+        return "redirect:/users";
     }
 
 }
--- src/main/java/ua/casten/user/project/service/RoleService.java
+++ src/main/java/ua/casten/user/project/service/RoleService.java
@@ -2,10 +2,12 @@ package ua.casten.user.project.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ua.casten.user.project.exception.NullEntityReferenceException;
 import ua.casten.user.project.model.Role;
 import ua.casten.user.project.model.repository.RoleRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoleService {
@@ -15,6 +17,17 @@ public class RoleService {
     @Autowired
     public RoleService(RoleRepository roleRepository) {
         this.roleRepository = roleRepository;
+    }
+
+    public Role readById(Long id) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("Id parameter less or equals 0.");
+        }
+        Optional<Role> role = roleRepository.findById(id);
+        if (role.isEmpty()) {
+            throw new NullEntityReferenceException("Null project object from DB.");
+        }
+        return role.get();
     }
 
     public List<Role> getAll() {
