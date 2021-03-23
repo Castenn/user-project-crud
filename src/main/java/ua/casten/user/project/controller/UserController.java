820b2ae9c9e90f9827665792a80b1151c97fef98
Oleksandr Borovyk <maingroon@gmail.com>
2021-03-23 21:52:17 +0200

    Added user-read functional

--- src/main/java/ua/casten/user/project/controller/UserController.java
+++ src/main/java/ua/casten/user/project/controller/UserController.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ua.casten.user.project.model.User;
 import ua.casten.user.project.service.UserService;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -19,10 +22,18 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
-    public String findAll(Model model) {
+    @GetMapping("")
+    public String getAll(Model model) {
         List<User> users = userService.getAll();
         model.addAttribute("users", users);
         return "user-list";
     }
+
+    @GetMapping("{id}/read")
+    public String getUserInfo(@PathVariable Long id, Model model) {
+        User user = userService.readById(id);
+        model.addAttribute("user", user);
+        return "user-read";
+    }
+
 }
