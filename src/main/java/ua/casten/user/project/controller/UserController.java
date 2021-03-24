bc76ce515583e91ad50376cf8794461b470f1528
Oleksandr Borovyk <maingroon@gmail.com>
2021-03-24 22:12:30 +0200

    Added ProjectController

--- src/main/java/ua/casten/user/project/controller/UserController.java
+++ src/main/java/ua/casten/user/project/controller/UserController.java
@@ -1,6 +1,5 @@
 package ua.casten.user.project.controller;
 
-import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
